87d3fd1388a576ec59989a42e061200199c78d58
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-05-02 21:01:33 +0600

    Hooks
    custom Hooks

---
+++ src/useHooks/useMenu.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useMenu = () => {
+    const [menu, setmenu] = useState([]);
+    const [loading, setloading] = useState(true);
+    useEffect(() => {
+       fetch('menu.json')
+       .then(res => res.json())
+       .then(data => {
+        setmenu(data)
+        setloading(false)
+    }) 
+    }, []);
+
+    return[menu,loading]
+};
+
+export default useMenu;

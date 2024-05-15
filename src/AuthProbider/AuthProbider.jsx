56ac2b76119889f6940c9daa2b746017d49abda6
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-05-15 21:37:31 +0600

    for 401 or 403 logOut the user and move the user to login

--- src/AuthProbider/AuthProbider.jsx
+++ src/AuthProbider/AuthProbider.jsx
@@ -1,83 +1,100 @@
-
-import {  createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile , GoogleAuthProvider } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/Firebase";
-
+import useAxiosPublic from "../useHooks/useAxiosPublic";
 
 export const AuthContex = createContext(null);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
+const AuthProbider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const AuthProbider = ({children}) => {
-    const [user , setUser] = useState(null);
-    const [loading , setLoading] = useState(true);
-    //google login
-    const provider = new GoogleAuthProvider(); 
+  // jwt -> 1
+  const axiosPublic = useAxiosPublic();
 
+  //google login
+  const provider = new GoogleAuthProvider();
 
-    // sing in user
-    const singIn = (email, pass)=>{
-        setLoading(true);
-        return signInWithEmailAndPassword(auth, email , pass)
-    }
-    // creater user 
-    const createuser = (email , pass) =>{
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth, email ,pass);
-    }
+  // sing in user
+  const singIn = (email, pass) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, pass);
+  };
+  // creater user
+  const createuser = (email, pass) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
 
-    //sign Out
-    const logout = () =>{
-        setLoading(true);
-        return signOut(auth);
-    }
+  //sign Out
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    //update user 
-    const updateUserProfile = (name, photo)=>{
-        return updateProfile(auth.currentUser, {
-            displayName: name, photoURL: photo
-        })    
-    }
+  //update user
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
-    // google SingIn 
+  // google SingIn
 
-    const googleSingin = () => {
-        setLoading(true)
-        return signInWithPopup(auth, provider)
-    }
+  const googleSingin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
 
-    // current user check
-    useEffect(() => {
-        const unsubcribe =  onAuthStateChanged ( auth , currentuser =>{
-            setUser(currentuser);
-            setLoading(false);
-            console.log('current user ', currentuser);
+  // current user check
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currentuser) => {
+      setUser(currentuser);
+
+      if (currentuser) {
+        //unik user defined 
+        const userInfo = { email: currentuser.email };
+        // get token storage client
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
         });
+      } else {
+        // remove token
+        localStorage.removeItem("access-token");
+      }
 
-        return () =>{
-            return unsubcribe();
-        }
+      setLoading(false);
+    });
 
-    }, []);
+    return () => {
+      return unsubcribe();
+    };
+  }, [axiosPublic]);
 
-    const authInfo = {
-        user,
-        loading,
-        singIn,
-        createuser,
-        googleSingin,
-        logout,
-        updateUserProfile
+  const authInfo = {
+    user,
+    loading,
+    singIn,
+    createuser,
+    googleSingin,
+    logout,
+    updateUserProfile,
+  };
 
-
-    }
-
-    return (
-        <AuthContex.Provider value={authInfo}>
-            {children}
-        </AuthContex.Provider>
-    );
-
+  return <AuthContex.Provider value={authInfo}>{children}</AuthContex.Provider>;
 };
 
 export default AuthProbider;
--- src/useHooks/useAxios.jsx
+++ src/useHooks/useAxios.jsx
@@ -1,10 +1,45 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSucure = axios.create({
     baseURL:"http://localhost:5000"
 });
 
 const useAxios = () => {
+    const naviget = useNavigate();
+    const {logout} = useAuth();
+    // jwt -> 4  -> interceptors -> all info axios web interceptors 
+    axiosSucure.interceptors.request.use(function (config){
+        // jwt -> 5
+        const token = localStorage.getItem('access-token')
+        console.log('rsbi', token);
+        // jwt -> 6
+        config.headers.authorization = `Bearer ${token}`
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+
+    //   interceptors 401 and 403 status 
+    axiosSucure.interceptors.response.use( (response) => {
+        return response;
+    } , async (error) =>{
+        // jwt -> 7
+        const status = error.response.status;
+        console.log(status);
+
+        // jwt -7
+        // for 401 or 403 logOut the user and move the user to login 
+        if(status === 401 || status === 403){
+            await logout();
+            naviget('/login')
+        }
+
+        return Promise.reject(error);
+    })
+
     return axiosSucure;
 };
 
